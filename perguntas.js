7ad1830ec0c9d18fa173eee4625c771221832809
yagocorreiasouza <00001113868107sp@al.educacao.sp.gov.br>
2024-12-03 14:14:00 -0300

    projeto final.

---
+++ perguntas.js
@@ -0,0 +1,54 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'Matemática',
+    'O que é um número primo?',
+    'É todo número divisível apenas por 1 e por ele mesmo'
+)
+
+criaCartao(
+    'Surpresa',
+    'Qual o nome do seu Professor de Tecnologia?',
+    'Jocimar Montanha'
+)
+
+    criaCartao(
+        'Biologia',
+        'O que é a Piscicultura?',
+        'É a produção comercial de peixes em ambientes controlados.'
+    )
+    
+    criaCartao(
+        'Educação Física',
+        'Quais são os fundamentos básicos do handebol?',
+        'Empunhadura, passe, recepção, arremesso, drible e finta'
+    )
+
+    criaCartao(
+        'Química',
+        'O que é a tabela periódica?',
+        'é uma ferramenta que tem por objetivo organizar e agrupar todos os elementos químicos já descobertos pelo ser humano'
+    )
+  
